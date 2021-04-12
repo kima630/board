cf97ececaf0e49acd92cea4295bf61c4defbb309
kima630 <hyun_0630_@naver.com>
2021-04-12 09:10:22 +0900

    board

---
+++ src/component/BoardMain.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BoardMain = () => {
+  return (
+    <div>
+      게시판
+      <input className="title" type="text" />
+    </div>
+  );
+};
+
+export default BoardMain;
